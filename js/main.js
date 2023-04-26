e4981ea488f5bc4c58b7e7d6840b15755411e47d
D-Games <8223329+D-Games@users.noreply.github.com>
2023-04-26 12:11:44 +0100

    Adding functionality to the button to open description div and fixing things that weren't working properly plus a bit of polish

--- js/main.js
+++ js/main.js
@@ -1,2 +1,73 @@
-    var dt = new Date();
-    document.getElementById("date-time").innerHTML=dt;
+var dt = new Date();
+document.getElementById("date-time").innerHTML=dt;
+    
+
+      function openDiv(id){
+        var descDiv = document.querySelector("#" + id);
+        var descText = document.getElementById("descText" + id.substr(-1));
+        var linkBtn = document.getElementById("linkButton" + id.substr(-1));
+        if (descDiv.style.height == "75%"){
+          descDiv.style.height = "0%";
+          descText.style.display = "none";
+          linkBtn.style.display = "none";
+        } else {
+          descDiv.style.height = "75%";
+          descText.style.display = "block";
+          descText.style.color = "white";
+          linkBtn.style.display = "block";
+          linkBtn.style.color = "white";
+        }
+      }
+
+      function setTextDisplay(){
+        var descText = document.getElementById("descText1");
+        descText.style.display = "none";
+      }
+
+      function test(){
+        var test = document.getElementById("button1")
+        test.style.backgroundColor = "red";
+
+      }
+
+
+/* CODE THAT DIDN'T WORK
+window.addEventListener("load", function() {
+  hideDescText();
+});
+
+window.addEventListener("load", function(){
+  setTextDisplay();
+  test();
+})*/
+      
+      /*function hideDescText(id) {
+        var descText = document.getElementById(id);
+        if (descText.style.display !== "none") {
+          descText.style.display = "none";
+        }
+      }
+      
+      window.onload = function() {
+        hideDescText("descText1");
+        hideDescText("descText2");
+      }
+
+        /*function openDiv(id) {
+        var descDiv = document.querySelector("#" + id);
+        var descText = document.querySelector(".descText1")
+        if (descDiv.style.height == "75%") {
+          descDiv.style.height = "0%";
+          descText.style.display = "none";
+        } else {
+          descDiv.style.height = "75%";
+          descText.style.display = "block";
+        }
+      }
+
+      function hideDescText() {
+        var descTextElements = document.querySelectorAll(".descText1");
+        for (var i = 0; i < descTextElements.length; i++) {
+          descTextElements[i].style.display = "none";
+        }
+      }*/
